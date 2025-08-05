c9cd55d338e01472fcf60c76fc23955d521f04fd
lidiyacheb <lidia.saulite@gmail.com>
2025-08-06 01:03:58 +0200

    add light animation into header text

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import "./Header.css";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 const HeroSection = () => {
+
+    const [offsetY, setOffsetY] = useState(0);
+
+    const handleScroll = () => setOffsetY(window.scrollY);
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-        <div className="hero-container">
+        <div
+            className="hero-container"
+            style={{
+                backgroundPositionY: `${offsetY * 0.5}px`
+            }}
+        >
             <div className="hero-overlay">
-                <h1 className="hero-title">Events planen - so einfach wie nie zuvor</h1>
+                <motion.h1
+                    className="hero-title"
+                    initial={{ opacity: 0, y: 50, scale: 0.8 }}
+                    animate={{ opacity: 1, y: 0, scale: 1 }}
+                    transition={{ duration: 1, ease: "easeOut" }} // smooth
+                >
+                    Events planen – so einfach wie nie zuvor
+                </motion.h1>
             </div>
         </div>
     );
