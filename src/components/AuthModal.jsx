e37d914961378caf4b7826548b508ed1b33d708b
lidiyacheb <lidia.saulite@gmail.com>
2025-08-06 17:48:06 +0200

    change modal form

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -1,21 +1,39 @@
-import { useState } from "react";
 import { AuthForm } from "../components/AuthForm";
 import { Dialog, DialogContent, IconButton } from "@mui/material";
 import { FaTimes } from "react-icons/fa";
 
 export function AuthModal({ mode, open, onClose, onSwitchMode }) {
     return (
-        <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
-            <DialogContent>
-                <div style={{ position: "relative" }}>
+        <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth
+                scroll="paper"
+                sx={{
+                    "& .MuiDialog-paper": {
+                        overflow: "hidden",
+                        borderRadius: "12px",
+                        padding: 0,
+                        margin: 0
+                    }
+                }}>
+            <DialogContent sx={{
+                position: "relative",
+                padding: "24px",
+                display: "flex",
+                flexDirection: "column"
+            }}>
                     <IconButton
                         onClick={onClose}
-                        style={{ position: "absolute", right: 0, top: 0 }}
+                        sx={{
+                            position: "absolute",
+                            top: "12px",
+                            right: "12px",
+                            zIndex: 10,
+                            color: "#666",
+                            "&:hover": { color: "#000" }
+                        }}
                     >
                         <FaTimes />
                     </IconButton>
                     <AuthForm mode={mode} onClose={onClose} onSwitchMode={onSwitchMode} />
-                </div>
             </DialogContent>
         </Dialog>
     );
